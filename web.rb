796834a59aa6bc6f4ec735d7687981734e5024de
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-06-13 08:53:08 -0700

    First successful deploy

--- web.rb
+++ web.rb
@@ -14,27 +14,9 @@ get '/' do
       :token => "351946902-APxV8nHCQM5TYbRX8jiYJpWvijTL1AKTnG405xIU",
       :secret => "vhdaIEHI26KJiyB6bxuN5026lAtLc3tKZt1aZOYs"
   )
-  @rate_limit_status = @client.rate_limit_status
 
-  #$nettuts_timeline = $twitteroauth->get('statuses/user_timeline', array('screen_name' => 'nettuts'));
+  @tweets = @client.user_timeline( { :screen_name => 'OSDDMalaria' } )
 
-  @timeline = @client.public_timeline( { :screen_name => 'cfitzpat' } )
-
-  #@client.url = "https://api.twitter.com/1/statuses/user_timeline/OSDDMalaria.json?callback=?&count=12&include_rts=1"
-
-  @tweets = @client.inspect
-  @methods = @client.methods
-  @user = @client.user
-  @ptl = @client.public_timeline
-
-  #@tweets = @client.inspect
   content_type :json
-  { :timeline => @timeline,
-    :ptl => @ptl,
-    :client => @client,
-    :methods => @methods,
-    :clientauthorized => @client.authorized?,
-    :client => @client,
-    :tweets => @tweets
-  }.to_json
+  { :tweets => @tweets }.to_json
 end
