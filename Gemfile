351fddb4e8058e200c86189224e910977cac692d
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-06-18 16:40:10 -0700

    Add project_activity endpoint

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'twitter_oauth'
 gem 'sinatra-jsonp'
+gem 'ruby-github'
--- web.rb
+++ web.rb
@@ -7,6 +7,7 @@ require 'sinatra'
 require 'twitter_oauth'
 require 'sinatra/jsonp'
 require 'json'
+require 'open-uri'
 
 get '/' do
   response.headers['Access-Control-Allow-Origin'] = '*'
@@ -23,3 +24,30 @@ get '/' do
   #content_type :json
   #{ :tweets => @tweets }.to_json
 end
+
+get '/sponsors_and_members' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+
+  members_file = "/tmp/members.json"
+  if File.exist?(members_file) && File.mtime(members_file) > (Time.now - 10*60)
+    @members = File.read(members_file)
+  else
+    @members = open("https://api.github.com/repos/OSDDMalaria/OSM_Website_Data/issues").read
+    File.write(members_file, @members)
+  end
+  jsonp @members
+end
+
+get '/project_activity' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+
+  project_activity_file = "/tmp/project_activity.json"
+  if File.exist?(project_activity_file) && File.mtime(project_activity_file) > (Time.now - 10*60)
+    @project_activity = File.read(project_activity_file)
+  else
+    @project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues").read
+    File.write(project_activity_file, @project_activity)
+  end
+  jsonp @project_activity
+end
+
