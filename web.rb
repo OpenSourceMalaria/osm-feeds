35c9e8ee698fc5cb0b37a73c326d15e5658303a9
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-06-24 09:02:05 -0700

    Return project activity in proper order

--- web.rb
+++ web.rb
@@ -85,7 +85,7 @@ get '/project_activity_combined' do
   @s
 end
 
-get '/project_activity' do
+get '/project_activity_old' do
   response.headers['Access-Control-Allow-Origin'] = '*'
 
   project_activity_file = "/tmp/project_activity.json"
@@ -116,27 +116,48 @@ get '/reset' do
 
 end
 
-get '/project_activity_test' do
+get '/project_activity' do
 
   response.headers['Access-Control-Allow-Origin'] = '*'
 
-  @combined = String.new
-  project_activity_file = "/tmp/project_activity.json"
+  if File.exist?(project_activity_file) && File.mtime(project_activity_file) > (Time.now - 10*60)
+    @back = File.read(project_activity_file)
+  else
+    @combined = String.new
+    project_activity_file = "/tmp/project_activity.json"
 
-    @open_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues", "UserAgent" => "Ruby-Wget").read
-    @closed_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues?state=closed", "UserAgent" => "Ruby-Wget").read
+      @open_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues", "UserAgent" => "Ruby-Wget").read
+      @closed_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues?state=closed", "UserAgent" => "Ruby-Wget").read
 
-    @combined << @open_project_activity
-    x = @combined.length
-    @combined[x-1] = ','
+      @combined << @open_project_activity
+      x = @combined.length
+      @combined[x-1] = ','
 
-    @closed_project_activity[0] = ' '
-    @combined << @closed_project_activity
+      @closed_project_activity[0] = ' '
+      @combined << @closed_project_activity
 
-    object_array = JSON.parse(@combined);
-    object_array = object_array.sort_by { |hsh| hsh["created_at"] }
-    object_array.reverse!
+      object_array = JSON.parse(@combined)
+      object_array = object_array.sort_by { |hsh| hsh["updated_at"] }
+      object_array.reverse!
 
-    @back = object_array.to_json
-    @back
+      for i in 0..11   # Only the top twelve are used
+        if object_array[i]["comments"] > 0
+          @comments = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues/"+ object_array[i]["number"].to_s + "/comments", "UserAgent" => "Ruby-Wget").read
+          comments_array = JSON.parse(@comments)
+          for j in 0..comments_array.length - 1
+            cdt = DateTime.parse (comments_array[j]["updated_at"])
+            odt = DateTime.parse (object_array[i]["updated_at"])
+            if cdt-odt > 0
+              object_array[i]["updated_at"] = comments_array[j]["updated_at"]
+            end
+          end
+        end
+      end
+
+      object_array = object_array.sort_by { |hsh| hsh["updated_at"] }
+      object_array.reverse!
+
+      @back = object_array.to_json
+  end
+  @back
 end
