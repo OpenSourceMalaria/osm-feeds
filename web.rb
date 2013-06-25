35f50bd1bd7f4b4133f1cf1831fc799a548ca20e
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-06-25 09:44:20 -0700

    Testing for project activity from GitHub
    Sorting
    
    WIP
    
    Use comment updated_dates
    
    WIP - write to local file
    
    to_json
    
    more to_json
    
    Remove dead code

--- web.rb
+++ web.rb
@@ -66,59 +66,39 @@ get '/project_activity' do
   response.headers['Access-Control-Allow-Origin'] = '*'
 
   project_activity_file = "/tmp/project_activity.json"
-  if File.exist?(project_activity_file) && File.mtime(project_activity_file) > (Time.now - 60*60)
-    @back = File.read(project_activity_file)
+  if File.exist?(project_activity_file) && File.mtime(project_activity_file) > (Time.now - 10*60)
+    $log.debug "Getting from file"
+    @combined = File.read(project_activity_file)
   else
-    @combined = String.new
+    $log.debug "Getting from GitHub"
+    @github = Octokit::Client.new({client_id: '9ec9caed6c4a85ff0798',
+                                   client_secret: 'cd437e96e33b5a6cb0b8e394f413cb9639b9fd8f'})
 
-    @open_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues", "UserAgent" => "Ruby-Wget").read
-    @closed_project_activity = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues?state=closed", "UserAgent" => "Ruby-Wget").read
+    @open_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'open'})
+    @closed_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'closed'})
 
-    @combined << @open_project_activity
-    x = @combined.length
-    @combined[x-1] = ','
+    @combined = @open_project_activity + @closed_project_activity
 
-    @closed_project_activity[0] = ' '
-    @combined << @closed_project_activity
+    @combined = @combined.sort_by { |hsh| hsh["updated_at"] }
 
-    object_array = JSON.parse(@combined)
-    object_array = object_array.sort_by { |hsh| hsh["updated_at"] }
-    object_array.reverse!
+    @combined.reverse!
 
     for i in 0..11   # Only the top twelve are used
-      if object_array[i]["comments"] > 0
-        @comments = open("https://api.github.com/repos/OSDDMalaria/OSDDMalaria_To_Do_List/issues/"+ object_array[i]["number"].to_s + "/comments", "UserAgent" => "Ruby-Wget").read
-        comments_array = JSON.parse(@comments)
-        for j in 0..comments_array.length - 1
-          cdt = DateTime.parse (comments_array[j]["updated_at"])
-          odt = DateTime.parse (object_array[i]["updated_at"])
+      if @combined[i]["comments"] > 0
+        @comments = @github.issue_comments("OSDDMalaria/OSDDMalaria_To_Do_List", @combined[i].number)
+        for j in 0..@comments.length - 1
+          cdt = DateTime.parse (@comments[j]["updated_at"])
+          odt = DateTime.parse (@combined[i]["updated_at"])
           if cdt-odt > 0
-            object_array[i]["updated_at"] = comments_array[j]["updated_at"]
+            @combined[i]["updated_at"] = @comments[j]["updated_at"]
           end
         end
       end
     end
-
-    object_array = object_array.sort_by { |hsh| hsh["updated_at"] }
-    object_array.reverse!
-
-    @back = object_array.to_json
+    @combined = @combined.sort_by { |hsh| hsh["updated_at"] }
+    @combined.reverse!
+    File.write(project_activity_file, @combined.to_json)
+    @combined = @combined.to_json
   end
-  @back
-end
-
-
-get '/project_activity_new' do
-
-  response.headers['Access-Control-Allow-Origin'] = '*'
-
-  @github = Octokit::Client.new({client_id: '9ec9caed6c4a85ff0798',
-                                 client_secret: 'cd437e96e33b5a6cb0b8e394f413cb9639b9fd8f'})
-
-  @open_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'open'})
-  @closed_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'closed'})
-
-  @combined = @open_project_activity << @closed_project_activity
-
-  @combined.to_json
+  @combined
 end
