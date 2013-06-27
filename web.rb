82cbe7dc48056088f2072f73dc1c2810ee3cc7d1
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2013-06-27 08:58:55 -0700

    Optimize

--- web.rb
+++ web.rb
@@ -14,9 +14,9 @@ require 'octokit'
 enable :logging
 
 before do
-  Dir.mkdir('logs') unless File.exist?('logs')
-  $log = Logger.new('logs/output.log','weekly')
-  $log.level = Logger::DEBUG
+  #Dir.mkdir('logs') unless File.exist?('logs')
+  #$log = Logger.new('logs/output.log','weekly')
+  #$log.level = Logger::DEBUG
 end
 
 get '/' do
@@ -63,34 +63,38 @@ end
 
 get '/project_activity' do
 
+  most_to_keep = 12;
+
   response.headers['Access-Control-Allow-Origin'] = '*'
 
   project_activity_file = "/tmp/project_activity.json"
   if File.exist?(project_activity_file) && File.mtime(project_activity_file) > (Time.now - 10*60)
-    $log.debug "Getting from file"
     @combined = File.read(project_activity_file)
   else
-    $log.debug "Getting from GitHub"
     @github = Octokit::Client.new({client_id: '9ec9caed6c4a85ff0798',
                                    client_secret: 'cd437e96e33b5a6cb0b8e394f413cb9639b9fd8f'})
 
     @open_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'open'})
+    @open_project_activity = @open_project_activity.take(most_to_keep)
+
     @closed_project_activity = @github.list_issues("OSDDMalaria/OSDDMalaria_To_Do_List", {state: 'closed'})
+    @closed_project_activity = @closed_project_activity.take(most_to_keep)
 
     @combined = @open_project_activity + @closed_project_activity
 
     @combined = @combined.sort_by { |hsh| hsh["updated_at"] }
-
     @combined.reverse!
+    @combined = @combined.take(most_to_keep)
 
-    for i in 0..11   # Only the top twelve are used
-      if @combined[i]["comments"] > 0
-        @comments = @github.issue_comments("OSDDMalaria/OSDDMalaria_To_Do_List", @combined[i].number)
-        for j in 0..@comments.length - 1
-          cdt = DateTime.parse (@comments[j]["updated_at"])
-          odt = DateTime.parse (@combined[i]["updated_at"])
+    @combined.each do |item|
+      # do whatever
+      if item["comments"] > 0
+        @comments = @github.issue_comments("OSDDMalaria/OSDDMalaria_To_Do_List", item.number)
+        @comments.each do |comment|
+          cdt = DateTime.parse (comment["updated_at"])
+          odt = DateTime.parse (item["updated_at"])
           if cdt-odt > 0
-            @combined[i]["updated_at"] = @comments[j]["updated_at"]
+            item["updated_at"] = comment["updated_at"]
           end
         end
       end
